031cf9ea95df9c1851e765409145bac15cef4db2
Romil Havewala <romilrh@gmail.com>
2017-02-12 18:24:42 -0600

    Implemented condition generation and verification

--- components/wordrush.ctr.js
+++ components/wordrush.ctr.js
@@ -24,27 +24,35 @@
             $scope.letters = String(letters.data).split(',')
         })
         
-        
         $scope.applicant = ''
         $scope.submittedWords = [] 
         $scope.submittedDisplay = ''
         $scope.amtCorrect = 0
-        $scope.wordCondition = 'start with "e"'
+        
         $scope.condition = {
             name: '',
             startsWith: '',
             endsWith: '',
             contains: '',
             length: 1,
-            containsLetter: '',
-            containsLetterTimes: 0
+            containsTimes: 0
         }
+        const SEPARATOR = ', and '
+        var finals = -1
+        
+        const MINLEN = 3
+        const MAXLEN = 6
+        
+        const MINTIMES = 1
+        const MAXTIMES = 3
         
         $scope.startGame = () => {
             $scope.gameOn = true
             $scope.virgin = false
             console.log('gameOn = ' + $scope.gameOn)
             console.log('virgin = ' + $scope.virgin)
+            
+            generateCondition()
             
             $scope.applicant = ''
             $scope.submittedWords = []
@@ -58,7 +66,6 @@
             $scope.submittedDisplay = $scope.submittedWords.join('\n')
         }
         
-
         $scope.isWord = element => {
 
             var minIndex = 0
@@ -84,35 +91,89 @@
 
         }
         
-        $scope.generateCondition = () => {
-            //condition.startsWith = randElement($scope.phrases)
+        function generateCondition() {
+            $scope.condition.startsWith = randElement($scope.startPhrases)
+            $scope.condition.endsWith = randElement($scope.endPhrases)
+            $scope.condition.contains = randElement($scope.inPhrases)
+            $scope.condition.length = randIn(MINLEN, MAXLEN)
+            $scope.condition.containsTimes = randIn(MINTIMES, MAXTIMES)
+            
+            $scope.condition.name = 'words that '
+            
+            var lower = 0
+            var upper = 2
+            for(let i = 0; i < 2; i++) {
+                
+                //randomly pick type of condition
+                finals = randIn(lower,upper)
+                lower = 3
+                upper = 3
+                
+                //add condition description to name
+                $scope.condition.name += 
+                    (finals === 0) ?
+                        'start with "' + $scope.condition.startsWith + '"' :
+                    (finals === 1) ?
+                        'end with "' + $scope.condition.endsWith + '"' :
+                    (finals === 2) ?
+                        'contain "' + $scope.condition.contains + '"' :
+                    //
+                        ' contain at least ' + $scope.condition.length + ' letters'
+                
+                $scope.condition.name += SEPARATOR 
+            }
+            
+            //delete separator from end
+            $scope.condition.name = $scope.condition.name.substring(0, $scope.condition.name.length-SEPARATOR.length)
         }
+        
+        $scope.meetsConditions = word => {
+            var checked = 
+                (finals === 0) ?
+                    word.startsWith($scope.condition.startsWith) :
+                (finals === 1) ?
+                    word.endsWith($scope.condition.endsWith) :
+                (finals === 2) ?
+                    word.includes($scope.condition.contains) :
+                //
+                    word.length >= $scope.condition.length
+            
+            console.log(word + ': checked = ' + checked)
+            console.log(word + ': is word = ' + $scope.isWord(word))
+            
+            return  checked && 
+                    /*$.inArray(word, $scope.submittedWords) === -1 &&*/
+                    $scope.isWord(word)
+        }
+        
+        $scope.submitWord = word => {
+            //on enter or spacebar
+            if(event.keyCode == (13) || event.keyCode == (32)) {
+                word = word.toLowerCase()
+                
+                //check if word has already been submitted
+                var used = $.inArray(word, $scope.submittedWords) !== -1
+                console.log(word + ': used = ' + used)
+                
+                $scope.submittedWords.push(word)
+                
+                $('#word-input').val('')
+                
+                console.log('Submitted ' + word)
+
+                if($scope.meetsConditions(word) && !used) {
+                    $scope.amtCorrect++
+                }
+            }
+        }
+        
         function randElement(arr) {
             var index = Math.floor(Math.random() * arr.length)
             return arr[index]
         }
         
-        $scope.meetsConditions = word => {
-            return $scope.startsWith(word, 'e') && 
-                    $.inArray(word, $scope.submittedWords) === -1 &&
-                    $scope.isWord(word)
-        }
-        
-        $scope.submitWord = word => {
-            if(event.keyCode == (13) || event.keyCode == (32)) {
-                word = word.toLowerCase()
-                $scope.submittedWords.push(word)
-                $('#word-input').val('')
-                console.log('Submitted ' + word)
-
-                if($scope.isWord(word) && $scope.meetsConditions(word)) {
-                    $scope.amtCorrect++
-                }
-            }
-        }
-
-        $scope.startsWith = (word,ex) => {
-            return word.toLowerCase().startsWith(ex)
+        function randIn(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
     })
 })()
--- components/wordrush.fac.js
+++ components/wordrush.fac.js
@@ -9,43 +9,30 @@
                 return $http.get('../data/valid-words.txt');
             }
 
-            return {
-                getValidWords : getValidWords
-            }
             
             function getStartPhrases() {
                 return $http.get('../data/valid-start-phrases.txt');
             }
 
-            return {
-                getStartPhrases : getStartPhrases
-            }
-            
             function getInPhrases() {
                 return $http.get('../data/valid-in-phrases.txt');
             }
 
-            return {
-                getInPhrases : getInPhrases
-            }
-            
             function getEndPhrases() {
                 return $http.get('../data/valid-end-phrases.txt');
             }
 
-            return {
-                getEndPhrases : getEndPhrases
-            }
-            
             function getLetters() {
                 return $http.get('../data/letters.txt');
             }
-
+           
             return {
-                getLetters : getLetters
+                getValidWords : getValidWords,
+                getLetters : getLetters,
+                getStartPhrases : getStartPhrases,
+                getInPhrases : getInPhrases,
+                getEndPhrases : getEndPhrases
             }
-            
-            
         })
 })();
 
