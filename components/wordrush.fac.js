b9c65efa5a859f8f359a772284b22e10c2d8333e
Romil Havewala <romilrh@gmail.com>
2017-03-24 14:03:51 -0500

    Reformatted code, finished suggestion

--- components/wordrush.fac.js
+++ components/wordrush.fac.js
@@ -1,47 +1,51 @@
 (function() {
-    'use strict';
-    
-    angular
-        .module("wordrush")
-        .factory("wordRushFac", function($http) {
+  'use strict';
 
-            function getValidWords() {
-                return $http.get('../data/valid-words.txt');
-            }
-        
-            function getLetterValues() {
-                return $http.get('../data/letter-values.json')
-            }
-            
-            function getStartPhrases() {
-                return $http.get('../data/valid-start-phrases.txt')
-            }
+  angular
+    .module("wordrush")
+    .factory("wordRushFac", function($http) {
 
-            function getInPhrases() {
-                return $http.get('../data/valid-in-phrases.txt');
-            }
+      function getValidWords() {
+        return $http.get('../data/valid-words.txt');
+      }
 
-            function getEndPhrases() {
-                return $http.get('../data/valid-end-phrases.txt');
-            }
+      function getLetterValues() {
+        return $http.get('../data/letter-values.json')
+      }
 
-            function getLetters() {
-                return $http.get('../data/letters.txt');
-            }
-        
-            function getCapPhrases() {
-                return $http.get('../data/valid-cap-phrases.txt');
-            }
-            
-            return {
-                getValidWords   : getValidWords,
-                getLetterValues : getLetterValues,
-                getLetters      : getLetters,
-                getStartPhrases : getStartPhrases,
-                getInPhrases    : getInPhrases,
-                getEndPhrases   : getEndPhrases,
-                getCapPhrases   : getCapPhrases
-            }
-        })
+      function getStartPhrases() {
+        return $http.get('../data/valid-start-phrases.txt')
+      }
+
+      function getInPhrases() {
+        return $http.get('../data/valid-in-phrases.txt');
+      }
+
+      function getEndPhrases() {
+        return $http.get('../data/valid-end-phrases.txt');
+      }
+
+      function getLetters() {
+        return $http.get('../data/letters.txt');
+      }
+
+      function getCapPhrases() {
+        return $http.get('../data/valid-cap-phrases.txt');
+      }
+
+      function getDefLink(word) {
+        return $http.get('http://www.dictionary.com/browse/' + word)
+      }
+
+      return {
+        getValidWords   : getValidWords,
+        getLetterValues : getLetterValues,
+        getLetters    : getLetters,
+        getStartPhrases : getStartPhrases,
+        getInPhrases  : getInPhrases,
+        getEndPhrases   : getEndPhrases,
+        getCapPhrases   : getCapPhrases,
+        getDefLink    : getDefLink
+      }
+    })
 })();
-
